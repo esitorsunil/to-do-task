596f13007c1ce0f283b8d8b5cdc262aa5a84988d
esitorsunil <sunilrio23@gmail.com>
2025-10-24 21:28:27 +0530

    backend setup: db, nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/db.js";
+dotenv.config();
 
 const app = express();
 
+
+app.get("/", (req, res) => {
+    res.send("Server is running")
+} )
+
 app.listen(5000, () => {
-    console.log("Server establish successfully at http://localhost:5000")
+    connectDb();
+    console.log("Server established successfully at http://localhost:5000")
 })
